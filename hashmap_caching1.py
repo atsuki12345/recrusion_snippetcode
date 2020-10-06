5b3f612105fa173525949f113b41cf87b6d88c67
github atsuki12345 <a.komatsu1990@gmail.com>
2020-10-06 18:54:05 +0900

    hashmap caching1 - Python/PHP

--- hashmap_caching1.py
+++ hashmap_caching1.py
@@ -1,8 +1,8 @@
 def existsWithinList(listL,dataY):
     hash_map = {}
     for i in range(int(len(listL))):
-        hash_map[listL[i]] = listL[i]
-    return False if hash_map.get(dataY) is None else True
+        hash_map[str(listL[i])] = listL[i]
+    return False if hash_map.get(str(dataY)) is None else True
 
 sampleList = [3,10,23,3,4,50,2,3,4,18,6,1,-2]
 print(existsWithinList(sampleList,23))
